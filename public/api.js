8f1ee9407a643cea0bc864166052980197e70ddd
Yuiko911 <101722573+Yuiko911@users.noreply.github.com>
2025-04-27 12:37:58 +0200

    ????????

--- public/api.js
+++ public/api.js
@@ -1,6 +1,6 @@
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.6.8/+esm';
 
-const API_BASE_URL = 'https://green-it-project-eight.vercel.app/api';
+const API_BASE_URL = '/api/index/';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
